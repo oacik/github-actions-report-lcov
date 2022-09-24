134f281c4d7fa9a258abac157339c8b2ee321170
Oguzcan Acik <oguzcan_acik@hotmail.com>
2022-09-24 12:22:14 +0200

    tests

--- dist/main/index.js
+++ dist/main/index.js
@@ -17312,7 +17312,13 @@ async function run() {
         )
 
         console.log('Update Comment ID: ' + existingComment.id);
-        await octokit.issues.updateComment({
+        // await octokit.issues.updateComment({
+        //   owner: github.context.repo.owner,
+        //   repo: github.context.repo.repo,
+        //   commentId: existingComment.id,
+        //   body: body,
+        // });
+        await octokit.request('PATCH /repos/${github.context.repo.owner}/${github.context.repo.repo}/issues/comments/${existingComment.id}', {
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
           commentId: existingComment.id,
